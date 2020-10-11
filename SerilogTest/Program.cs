a6ef48c519f1e4c08852d8fb85bf9a88a814ad26
범희 조 <afsdzvcx123@naver.com>
2020-10-11 12:14:32 +0800

    초기 커밋.

---
+++ SerilogTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using Serilog;
+using System;
+
+namespace SerilogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitSerilogTest();
+            Console.WriteLine();
+            CreateFileSerilogTest();
+        }
+
+        //Serilog 초기 기본 설정 및 사용법
+        private static void InitSerilogTest()
+        {
+            using (var log = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger())
+            {
+                log.Information("Hello, Serilog!");
+                log.Warning("Goodbye, Serilog.");
+            }
+        }
+
+        //Log 파일에 기록
+        private static void CreateFileSerilogTest()
+        {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File(@"c:\log\log_.txt",
+                rollingInterval: RollingInterval.Day,
+                rollOnFileSizeLimit: true)
+            .CreateLogger();
+
+
+            for (int idx = 0; idx < 10; idx++)
+            {
+                Log.Information("Hello, Serilog!");
+            }
+
+            Log.CloseAndFlush();
+        }
+    }
+}
